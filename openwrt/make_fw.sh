adae707e769f2cd3fb012e91083bffe970a01fc8
Danja Vasiliev <danja@k0a1a.net>
2014-09-29 21:39:28 +0200

    image builder script update

--- openwrt/make_fw.sh
+++ openwrt/make_fw.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-_PWD=$(pwd)
+## Superglue firmware image builder script
+## http://superglue.it | Danja Vasiliev, 2014
+##
+##  Requirements: 
+##     OpenWRT ImageBuilder blob:
+##        http://downloads.openwrt.org/barrier_breaker/14.07-rc3/ar71xx/generic 
+##        or http://downloads.openwrt.org/snapshots/trunk/ar71xx
+##     Superglue serverfiles repo (which this script is part of):
+##        http://git.superglue.it/superglue/serverfiles/tree/master
 
+_PWD=$(pwd)
 _IMAGEBUILDER="$_PWD/../../../openwrt/OpenWrt-ImageBuilder-ar71xx_generic-for-linux-x86_64"
 _BUILDS="$_PWD/../../../sg-builds"
+
+[[ -e $_IMAGEBUILDER ]] || echo 'ImageBuilder is missing'; exit 1
+[[ -e $_BUILDS ]] || echo 'Builds directory is missing'; exit 1
 
 ## dirs with platform specific files
 _TARGETS='DIR505A1 TLWR710'
@@ -23,7 +35,7 @@ if [[ -e sg_$_MAJOR.revision ]]; then
 else _MINOR=0
 fi
 
-## get OpenWRT verison
+## get OpenWRT revision number
 _OPENWRT=$(fgrep -m1 'REVISION:=' $_IMAGEBUILDER/include/version.mk || echo 'r00000')
 _OPENWRT=${_OPENWRT/REVISION:=/}
 echo $_OPENWRT > openwrt.revision
@@ -54,10 +66,11 @@ for _TARGET in $_TARGETS; do
   md5sum *.bin > md5sums
   cd -
 
-  if [[ $? -eq 0 ]]; then 
+  _ERR=$?
+
+  if [[ $_ERR -eq 0 ]]; then 
     echo -e "\n$_TARGET build completed\n"
   else
-    _ERR=$?
     rm -Rf $_BUILDS/$_VERSION/$_TARGET
   fi
 
@@ -71,7 +84,7 @@ done
 if [[ $_ERR -eq 0 ]]; then
   ## if build succeeded bump revision
   echo $_MINOR > sg_$_MAJOR.revision
-  echo -e "\nSUCCESS\n"
+  echo -e "\nBuild SUCCESS\n"
 
   ## create symlinks to latest
   [[ -e $_BUILDS/latest ]] && touch $_BUILDS/latest || mkdir $_BUILDS/latest 
@@ -83,8 +96,7 @@ if [[ $_ERR -eq 0 ]]; then
   done
 
 else
-  echo -e "\nFAILED\n"
+  echo -e "\nBuild FAIL\n"
 fi
 
 exit $_ERR
- 
